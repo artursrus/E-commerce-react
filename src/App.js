85c27ffbf8d8cbc1e2fdc7597b6a67d7b96692d6
artursrus <arturs.ruskulis1@gmail.com>
2023-11-07 16:52:26 +0000

    Basic folder layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./AppRoutes";
+import { Header } from "./components/Header";
 
-function App() {
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <AppRoutes />
+    </BrowserRouter>
+
   );
 }
-
-export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Main.css"
+export const Header = () => {
+  return (
+    <header>
+        <nav>
+            <div className="logo">
+
+            </div>
+        </nav>
+    </header>
+  )
+}
